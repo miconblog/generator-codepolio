8ffadc58aad091565b82f7b5a3b6c39d668a3315
jaejin.yun <spyrogira256@gmail.com>
2015-03-02 17:49:34 +0900

    add mocha test

--- app/index.js
+++ app/index.js
@@ -15,7 +15,7 @@ var HubpolioGenerator = generators.Base.extend({
   info : function(){
     if (!this.options['skip-welcome-message']) {
       this.log(yosay(
-        chalk.red('Welcome!') + '\n' + 
+        chalk.red('Welcome!') + '\n' +
         chalk.yellow('Now you can generate your portfolio website!')
       ));
     }
@@ -28,7 +28,7 @@ require('./src/1initializing')(HubpolioGenerator);
 require('./src/2prompts')(HubpolioGenerator);
 require('./src/3configuring')(HubpolioGenerator);
 require('./src/4default')(HubpolioGenerator);
-//require('./src/5writing')(HubpolioGenerator);
+require('./src/5writing')(HubpolioGenerator);
 require('./src/6conflicts')(HubpolioGenerator);
 require('./src/7install')(HubpolioGenerator);
 require('./src/8end')(HubpolioGenerator);
--- test/test.js
+++ test/test.js
@@ -1,9 +1,42 @@
-var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+var assert = require("assert");
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-generator').assert;
+
+var path = require('path');
+
+
+describe('Hublipolio Generator', function(){
+
+  describe('run test', function(){
+
+    var runGen;
+
+    var expected = [
+      'package.json',
+      'bower.json',
+      '.editorconfig',
+      '.jshintrc',
+      'src',
+      'gulp',
+      'gulpfile.js'
+  ];
+
+    beforeEach(function () {
+      runGen = helpers
+        .run(path.join(__dirname, '../app'))
+        .inDir(path.join(__dirname, '.tmp'))
+        .withPrompts({'repositories':['Github','Bitbuckte'],'theme':'basic'})
+        .withGenerators([[helpers.createDummyGenerator(), 'mocha:app']]);
+    });
+
+    it("created expected files",function(done) {
+      runGen.on('end',function() {
+        assert.file(expected);
+        done();
+      })
+    });
+
+  }) ;
+
+
+});
