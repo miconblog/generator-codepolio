26c241845dcb7bc1506a3f6c8afb011ea6057b17
jaejin.yun <spyrogira256@gmail.com>
2015-03-05 10:45:58 +0900

    #6 FIX input user account information during prompting
    FIXED.
    
    but we have to correct prompt message.

--- app/src/2prompts.js
+++ app/src/2prompts.js
@@ -24,10 +24,53 @@ module.exports = function (Generator) {
 
       }.bind(this));
     },
+    requestUsername: function(){
 
-    second: function(){
-      console.log("TODO:// input user account info");
-    }
+      if(  this.props.repositories != undefined &&  this.props.repositories.length > 0 ) {
+
+        var requestUsername = [];
+        _.forEach(this.props.repositories,function(v) {
+          requestUsername.push({
+            "type" : "input",
+
+            "name" : v,
+            "message" : "insert "+v+" username"
+          });
+        });
+
+        var done = this.async();
+        this.prompt(requestUsername, function(answers){
+          _.merge(this.props,answers);
+
+          done();
+
+        }.bind(this));
+
+      }
+
+    }  ,
+    requestPassword: function(){
+
+      if(  this.props.repositories != undefined &&  this.props.repositories.length > 0 && this.props.repositories.indexOf('Teamforge') > -1) {
+        var done = this.async();
+        this.prompt([
+          {
+            "type" : "password",
+
+            "name" : "teamforge_password",
+            "message" : "insert your password"
+          }], function(answers){
+
+          _.merge(this.props,answers);
+
+          done();
+
+        }.bind(this));
+
+      }
+
+    }  ,
+
 
   }
 }
